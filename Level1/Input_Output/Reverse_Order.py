4d1225fb4659efc2dd1cefe785abd126fc81ca90
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-01-21 20:45:50 +0530

    Reverse_Order

---
+++ Level1/Input_Output/Reverse_Order.py
@@ -0,0 +1,10 @@
+# Write a program to read and print 2 numbers. The numbers should be displayed in reverse order. First number should be printed second and second number should be printed first.
+
+# For example:
+
+# Input	Result
+# 5 6     6 5
+
+
+a,b=map(int,input().split())
+print(b,a)
