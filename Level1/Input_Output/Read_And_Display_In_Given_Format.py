7b6a7b49d53d3430be509fba4ae54ef90dc9f682
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-01-21 20:45:02 +0530

    Read_And_Display_In_Given_Format

---
+++ Level1/Input_Output/Read_And_Display_In_Given_Format.py
@@ -0,0 +1,11 @@
+# Write a program to read a number and display in given format
+
+# For example:
+
+# Input	Result
+# 5       Entered number is 5
+# 7       Entered number is 7
+
+
+a=int(input())
+print("Entered number is",a)
