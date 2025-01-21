d9cff62684030a8b173171537e21a58e96941e24
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-01-21 20:46:16 +0530

    Reverse_Order_With_Sentence

---
+++ Level1/Input_Output/Reverse_Order_With_Sentence.py
@@ -0,0 +1,11 @@
+# Read the 2 numbers in order and display in reverse order
+
+# For example:
+
+# Input	Result
+# 3       Second is 4 and First is 3
+# 4
+
+a=int(input())
+b=int(input())
+print(f"Second is {b} and First is {a}")
