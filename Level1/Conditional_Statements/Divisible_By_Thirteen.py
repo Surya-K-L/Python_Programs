5d4ec8f3c2f81ad49daa254f5ea559ebce7f3893
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-01-22 21:11:20 +0530

    Divide_By_Thirteen

---
+++ Level1/Conditional_Statements/Divisible_By_Thirteen.py
@@ -0,0 +1,13 @@
+# Write a program to find if a given number is divisible by 13
+
+# For example:
+
+# Input	Result
+# 39      YES
+# 43      NO
+
+a=int(input())
+if(a%13==0):
+    print("YES")
+else:
+    print("NO")
