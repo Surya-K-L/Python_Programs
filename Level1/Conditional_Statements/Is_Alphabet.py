889aee854892307306825b38eec9761bd7939fac
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-01-22 21:13:05 +0530

    Is_ALphabet

---
+++ Level1/Conditional_Statements/Is_Alphabet.py
@@ -0,0 +1,13 @@
+# Design a Program to check whether a character is alphabet or not.
+
+# For example:
+
+# Input	Result
+# f       f is an alphabet
+# !       ! is not an alphabet
+
+c=input()
+if c.isalpha():
+    print(f"{c} is an alphabet")
+else:
+    print(f"{c} is not an alphabet")
