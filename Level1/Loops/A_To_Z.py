5e051fbd28ad3be4c9ed38b0ee7dbd8e782d0abb
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-01-23 22:42:51 +0530

    Create A_To_Z.py

---
+++ Level1/Loops/A_To_Z.py
@@ -0,0 +1,11 @@
+# Write a program to print alphabets from A to Z.
+
+#  Sample Output
+
+# A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+
+
+for z in range(65,91):
+    print(chr(z),end=" ")
+    
