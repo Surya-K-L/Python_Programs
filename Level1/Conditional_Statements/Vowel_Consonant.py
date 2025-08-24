9cb34e5fcd526476a73848bfbd7ad58fdd2d5a39
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-08-24 20:02:07 +0530

    Create Vowel_Consonant.py

---
+++ Level1/Conditional_Statements/Vowel_Consonant.py
@@ -0,0 +1,7 @@
+a=input()
+if(a=='A' or a=='E' or a=='I' or a=='O' or a=='U' or a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+    print(a,"is a Vowel")
+else:
+    print(a,"is a Consonant")
+
+    
